71be78a11600d6d14b937006f4fdd4dfd1124cec
TwistedWonder2021 <124842528+Dcar0803@users.noreply.github.com>
2024-12-08 22:29:21 -0500

    Test: Created test function for returning all numbers in the parameter list

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,12 +1,22 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class Testing {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	 @Test
+	    public void testGetEvenNumbers() {
+	        
+		 	List<Integer> input = Arrays.asList(1, 2, 3, 4, 5, 6);
+	        
+		 	List<Integer> expected = Arrays.asList(2, 4, 6);
 
-}
+	        List<Integer> actual = Project2.getEvenNumbers(input);
+
+	        assertEquals(expected, actual, "getEvenNumbers() did not return the expected list of even numbers.");
+	    }//end of testGetEvenNumbers method
+
+}//end of class
